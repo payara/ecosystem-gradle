0b11eb4acfd0eea5b35031da6c9870b4c4654ca7
Gaurav Gupta <gaurav.gupta.jc@gmail.com>
2019-05-30 11:11:12 +0530

    GP-17 Updated default version number to 5.192

--- payara-micro-gradle-plugin/src/main/java/fish/payara/gradle/plugins/micro/Configuration.java
+++ payara-micro-gradle-plugin/src/main/java/fish/payara/gradle/plugins/micro/Configuration.java
@@ -44,7 +44,7 @@ public interface Configuration {
     String MICRO_THREAD_NAME = "PayaraMicroThread";
     String MICRO_READY_MESSAGE = "ready in";
 
-    String DEFAULT_MICRO_VERSION = "5.191";
+    String DEFAULT_MICRO_VERSION = "5.192";
 
     String JAR_EXTENSION = "jar";
     String WAR_EXTENSION = "war";

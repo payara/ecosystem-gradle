d1d6c269fe63030ff28ac9859b7fc201e2053c82
Gaurav Gupta <gaurav.gupta.jc@gmail.com>
2019-07-22 11:51:45 +0530

    GP-5 Payara Micro Gradle Plugin Travis CI setup for automatic testing

--- payara-micro-gradle-plugin/src/test/java/fish/payara/gradle/plugins/micro/BaseTest.java
+++ payara-micro-gradle-plugin/src/test/java/fish/payara/gradle/plugins/micro/BaseTest.java
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2018 Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) [2018-2019] Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -38,6 +38,9 @@
  */
 package fish.payara.gradle.plugins.micro;
 
+import java.util.concurrent.TimeUnit;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import org.awaitility.Awaitility;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.internal.artifacts.dsl.DefaultRepositoryHandler;
@@ -118,7 +121,8 @@ public abstract class BaseTest {
         assertNotNull(stopTask.findProcessId());
 
         stopTask.execute();
-        assertNull(stopTask.findProcessId());
+        Awaitility.await()
+                .until(() -> stopTask.findProcessId() == null);
     }
 
 }

8a13c38388b19af06906787497c038e4ccb112c3
Gaurav Gupta <gaurav.gupta@payara.fish>
2021-08-10 11:47:30 +0530

    FISH-1352 Payara Micro version upgrade to 5.2021.5

--- payara-micro-gradle-plugin/src/main/java/fish/payara/gradle/plugins/micro/Configuration.java
+++ payara-micro-gradle-plugin/src/main/java/fish/payara/gradle/plugins/micro/Configuration.java
@@ -44,7 +44,7 @@ public interface Configuration {
     String MICRO_THREAD_NAME = "PayaraMicroThread";
     String MICRO_READY_MESSAGE = "ready in";
 
-    String DEFAULT_MICRO_VERSION = "5.201";
+    String DEFAULT_MICRO_VERSION = "5.2021.5";
 
     String JAR_EXTENSION = "jar";
     String WAR_EXTENSION = "war";
